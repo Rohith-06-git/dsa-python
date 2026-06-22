483280975aeb5a55d4ed025155c1407b5975cc75
Rohith Chandra <rohithchandra12345@gmail.com>
2026-06-23 00:25:42 +0530

    Solved few problems

---
+++ sliding_window/Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxVowels(self, s: str, k: int) -> int:
+        left = 0 
+        vowels = "aeiou"
+        max_count = 0
+        count = 0
+        for right in range(len(s)) :
+            if s[right] in vowels :
+                count += 1
+            if right - left + 1 == k :
+                max_count = max(max_count , count)
+                if max_count == k :
+                    return k
+                if s[left] in vowels :
+                    count -= 1
+                left += 1
+            
+        return max_count 
